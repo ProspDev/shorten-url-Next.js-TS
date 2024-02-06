f3b0005aa7e9cacca5d28142515f1dc055e8b29f
ProspDev <prosper.shine0714@gmail.com>
2024-02-06 10:37:35 +0000

    style: font sans added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import type { NextPage } from "next";
+import MainLayout from "layouts/MainLayout";
 
 const Home: NextPage = () => {
-  return <div>Hello World!</div>;
+  return (
+    <MainLayout pageTitle="Home">
+      <p className="text-center">Hello World</p>
+    </MainLayout>
+  );
 };
 
 export default Home;
